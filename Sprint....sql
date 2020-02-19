2d54b5dec6aa44739bc53ec565465360a4ee7e93
Okuhle <blessing.ntloyiya@gmail.com>
2020-02-19 11:17:19 +0200

    Add files via upload

---
+++ Sprint....sql
@@ -0,0 +1,62 @@
+/*
+-- Query: select Products from world.sprint
+LIMIT 0, 1000
+
+-- Date: 2020-02-19 10:48
+*/
+INSERT INTO `` (`Products`) VALUES ('Doritos');
+INSERT INTO `` (`Products`) VALUES ('Lays');
+INSERT INTO `` (`Products`) VALUES ('NikNaks');
+INSERT INTO `` (`Products`) VALUES ('Simba');
+INSERT INTO `` (`Products`) VALUES ('');
+INSERT INTO `` (`Products`) VALUES ('Fanta');
+INSERT INTO `` (`Products`) VALUES ('Sprite');
+INSERT INTO `` (`Products`) VALUES ('Jive');
+INSERT INTO `` (`Products`) VALUES ('Score');
+INSERT INTO `` (`Products`) VALUES ('');
+INSERT INTO `` (`Products`) VALUES (' Ps');
+INSERT INTO `` (`Products`) VALUES ('KitKat');
+INSERT INTO `` (`Products`) VALUES ('Bar-one');
+INSERT INTO `` (`Products`) VALUES ('Tex');
+INSERT INTO `` (`Products`) VALUES ('');
+INSERT INTO `` (`Products`) VALUES ('Chicken');
+INSERT INTO `` (`Products`) VALUES ('Burger');
+INSERT INTO `` (`Products`) VALUES ('steak');
+INSERT INTO `` (`Products`) VALUES ('pepper');
+INSERT INTO `` (`Products`) VALUES ('');
+INSERT INTO `` (`Products`) VALUES ('Guava');
+INSERT INTO `` (`Products`) VALUES ('Peach');
+INSERT INTO `` (`Products`) VALUES ('Pear');
+INSERT INTO `` (`Products`) VALUES ('Banana');
+INSERT INTO `` (`Products`) VALUES ('');
+INSERT INTO `` (`Products`) VALUES ('Vanilla');
+INSERT INTO `` (`Products`) VALUES ('Chocmint');
+INSERT INTO `` (`Products`) VALUES ('Browny');
+INSERT INTO `` (`Products`) VALUES ('Chocolate');
+INSERT INTO `` (`Products`) VALUES ('');
+INSERT INTO `` (`Products`) VALUES ('Potatoes');
+INSERT INTO `` (`Products`) VALUES ('Spinach');
+INSERT INTO `` (`Products`) VALUES ('Onion');
+INSERT INTO `` (`Products`) VALUES ('Butternut');
+INSERT INTO `` (`Products`) VALUES ('Lo');
+INSERT INTO `` (`Products`) VALUES ('la');
+INSERT INTO `` (`Products`) VALUES ('la');
+INSERT INTO `` (`Products`) VALUES ('Lo');
+INSERT INTO `` (`Products`) VALUES ('la');
+INSERT INTO `` (`Products`) VALUES ('la');
+INSERT INTO `` (`Products`) VALUES ('Lo');
+INSERT INTO `` (`Products`) VALUES ('la');
+INSERT INTO `` (`Products`) VALUES ('la');
+INSERT INTO `` (`Products`) VALUES ('Lo');
+INSERT INTO `` (`Products`) VALUES ('la');
+INSERT INTO `` (`Products`) VALUES ('la');
+INSERT INTO `` (`Products`) VALUES ('Lo');
+INSERT INTO `` (`Products`) VALUES ('la');
+INSERT INTO `` (`Products`) VALUES ('la');
+INSERT INTO `` (`Products`) VALUES ('Lo');
+INSERT INTO `` (`Products`) VALUES ('la');
+INSERT INTO `` (`Products`) VALUES ('la');
+INSERT INTO `` (`Products`) VALUES ('Lo');
+INSERT INTO `` (`Products`) VALUES ('la');
+INSERT INTO `` (`Products`) VALUES ('la');
+INSERT INTO `` (`Products`) VALUES (NULL);
